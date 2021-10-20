6eca804e972c25b9c6635ec364e9dece0a2b7101
eliozashvili <ilivhsazoile.g@gmail.com>
2021-10-21 00:10:45 +0400

    Changed code to a better version

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ clickMe.addEventListener('click', function () {
     photo.removeAttribute('hidden');
 });
 
-let count = 1;
+const img = ['img_1.png', 'img_2.png', 'img_3.png', 'img_4.png'];
+let count = 0;
+
 photo.addEventListener('click', function () {
+    if (count === img.length)
+        count = 0;
+
+    photo.src = img[count];
     count++;
-
-    if (count === 5) {
-        count = 1;
-    }
-
-    photo.src = `img_${count}.png`;
 });
